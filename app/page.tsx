e6e3e557064dc75e162b1e0ba253e766540ca660
gabrielbm7 <gabriel.minharo@hotmail.com>
2025-08-12 00:28:04 +0000

    chore: força novo deploy

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Site no ar!
         </a>
       </footer>
     </div>
