21ee9a80a9d2b58b2ab3aa1468ee8b40b75b219a
gabrielbm7 <gabriel.minharo@hotmail.com>
2025-08-12 07:12:10 +0000

    fix(api): usa strings para enums do Prisma (HOLD/CONFIRMADO, ONLINE/PRESENCIAL)

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { PrismaClient, ApptStatus, SessaoTipo } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
